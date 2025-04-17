b68e1ff5332ee4d4f2dfe71c407bc8d2696c5450
Jhan-Pierre <jhanpierredev@gmail.com>
2025-04-17 17:21:51 -0500

    Se actualizo el conteo de certificados.

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -3,5 +3,5 @@ import type { Achievement } from "@/types"
 export const achievements: Achievement[] = [
   { value: "+1", label: "AÑOS DE EXPERIENCIA" },
   { value: "+3", label: "PROYECTOS COMPLETADOS" },
-  { value: "+23", label: "CERTIFICADOS OBTENIDOS" },
+  { value: "+26", label: "CERTIFICADOS OBTENIDOS" },
 ]
